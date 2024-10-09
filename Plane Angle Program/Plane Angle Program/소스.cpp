6dee57530a07e2763e6a8af8df8bb3976d80b7f1
mandu38 <whxogus4793@naver.com>
2024-10-09 22:34:55 +0900

    d
    d

--- Plane Angle Program/Plane Angle Program/소스.cpp
+++ Plane Angle Program/Plane Angle Program/소스.cpp
@@ -2,28 +2,28 @@
 #include <math.h> 
 
 
-int main(void)
+int main(void) 
 {
 		
-	double PlaneAngle;
+	double PlaneAngle; //변수 선언
 	double ConvertAngle;
 
 
-	PlaneAngle = 0;
+	PlaneAngle = 0; //변수 초기화
 	ConvertAngle = 0;
 	
 
-	printf("변환할 실수형 자세값을 입력하세요 [deg]\n");
-	scanf_s("%lf", &PlaneAngle);
+	printf("변환할 실수형 자세값을 입력하세요 [deg]\n"); 
+	scanf_s("%lf", &PlaneAngle); //자세값 입력받기
 
 
-	PlaneAngle = (PlaneAngle > 180) * (PlaneAngle - 360) + (PlaneAngle <= 180) * PlaneAngle;
-	PlaneAngle = (PlaneAngle < -180) * (PlaneAngle + 360) + (PlaneAngle >= -180) * PlaneAngle;
+	PlaneAngle = (PlaneAngle > 180) * (PlaneAngle - 360) + (PlaneAngle <= 180) * PlaneAngle; //입력값이 180 초과할경우 360을 뺌. 아닐시 그대로 출력
+	PlaneAngle = (PlaneAngle < -180) * (PlaneAngle + 360) + (PlaneAngle >= -180) * PlaneAngle; //입력값이 180 미만일경우 360을 뺌. 아닐시 그대로 출력
 
-	ConvertAngle = fmod(PlaneAngle,180);
+	ConvertAngle = fmod(PlaneAngle,180); //fmod함수를 통해 실수형 변수인 PlanAngle의 나머지를 구하고 180으로 나눈 나머지값을 ConvertAngle 변수에 저장
 
-	printf("변환된 자세값 : %.3lf 도", ConvertAngle);
+	printf("변환된 자세값 : %.3lf 도", ConvertAngle); //계산된 값을 출력
 
-	return 0;
+	return 0; //프로그램 종료
 
 }
--- calculate/calculate/소스.cpp
+++ calculate/calculate/소스.cpp
@@ -4,7 +4,7 @@
 int main(void)
 {
 
-	int Thousand;
+	int Thousand; //변수선언 
 	int FiveHundred;
 	int Hundred;
 	double Amount;
@@ -13,7 +13,7 @@ int main(void)
 	double ChangeRemainder;
 	double FiveRemainder;
 
-	Amount = 0;
+	Amount = 0;  //변수 초기화
 	Object = 0;
 	Change = 0;
 	Thousand = 0;
@@ -23,19 +23,25 @@ int main(void)
 	FiveRemainder = 0;
 
 
-	printf("투입한 금액과 물건값을 차례대로 입력해주세요.\n");
-	scanf_s("%lf%lf", &Amount, &Object);
+	printf("투입한 금액과 물건값을 차례대로 입력해주세요.\n"); // 입력받을 값에 대한 문장 출력 
+	scanf_s("%lf%lf", &Amount, &Object); // 값 입력받고 변수에 저장
 
-	Change = Amount - Object;
-	Change = ((Change > 0) * Amount) - ((Change > 0) * Object);
-	Thousand = Change / 1000; 
-	ChangeRemainder = fmod(Change, 1000);
-	FiveRemainder = fmod(ChangeRemainder,500);
-	FiveHundred = ChangeRemainder / 500;
-	Hundred = FiveRemainder / 100;
+	Change = Amount - Object; // 금액과 물건값을 빼 Change 변수에 저장
 
-	printf("필요한 거스름돈 : %.1lf\n 천원 %d장\n 오백원 %d개\n 백원 %d개\n", Change, Thousand, FiveHundred, Hundred);
+	Change = ((Change > 0) * Amount) - ((Change > 0) * Object); //거스름돈이 역으로 음수값이 나오거나 잘못입력되는 상황 방지
 
-	return 0;
+	Thousand = Change / 1000; // 거스름돈을 1000으로 나누어 천원의 장수를 구하고 Thousand 변수에 저장 
+
+	ChangeRemainder = fmod(Change, 1000); //fmod 함수를 통해 거스름돈을 1000으로 나눴을때 나머지값을 ChangeRemainder에 저장
+
+	FiveRemainder = fmod(ChangeRemainder,500); //fmod 함수를 통해ChangeRemainder에 저장된 값을 500으로 나눴을때 나머지값을 FiveRemainder에 저장
+
+	FiveHundred = ChangeRemainder / 500; //거스름돈을 1000으로 나눈  ChangeRemainder에 저장된 값을 500으로 나누어 500원의 갯수 계산하고 FiveHundred에 저장
+
+	Hundred = FiveRemainder / 100; // FiveRemainder에 저장된 나머지값을 100으로 나누어 100원의 갯수를 계산하고 출력
+
+	printf("필요한 거스름돈 : %.1lf\n 천원 %d장\n 오백원 %d개\n 백원 %d개\n", Change, Thousand, FiveHundred, Hundred); //각각 계산된 거스름돈,천원의 장수, 오백원의 갯수, 백원의 갯수 출력 
+
+	return 0; //프로그램 종료
 
 }
